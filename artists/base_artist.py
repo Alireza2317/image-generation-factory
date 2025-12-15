96515c7e8a1332b764b28514935bc3c2716140f3
Alireza <alireza.alavi237@gmail.com>
2025-12-16 00:50:00 +0100

    seperating general conf from pain conf

--- artists/base_artist.py
+++ artists/base_artist.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 class Artist(ABC):
-	def __init__(self, config: dict[str, str | int]) -> None:
+	def __init__(self, config: dict[str, Any]) -> None:
 		self.config = config
 
 	@abstractmethod
-	def paint(self, prompt: str, image_name: str) -> bool:
+	def paint(self, prompt: str, image_name: str, paint_cfg: dict[str, Any]) -> bool:
 		'''Returns True if image generation was successful, else False'''
 		pass

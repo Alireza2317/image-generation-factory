f389498ef388405721671ae3b228492e2fd1d6a4
Alireza <alireza.alavi237@gmail.com>
2025-12-15 04:00:36 +0100

    updated base artist paint method signiture

--- artists/base_artist.py
+++ artists/base_artist.py
@@ -4,6 +4,6 @@ from pathlib import Path
 
 class Artist(ABC):
 	@abstractmethod
-	def paint(self, prompt: str, output_path: Path) -> bool:
+	def paint(self, prompt: str, ar: str, output_path: Path) -> bool:
 		'''Returns True if image generation was successful, else False'''
 		pass

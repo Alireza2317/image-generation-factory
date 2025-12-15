1322b8f24873a7504639e7f1efde8be1c7cbb814
Alireza <alireza.alavi237@gmail.com>
2025-12-16 00:59:59 +0100

    integrated config seperation to BananaArtist

--- artists/artist_banana.py
+++ artists/artist_banana.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Any
 from artists.base_artist import Artist
 from google import genai
 from google.genai.types import (
@@ -6,40 +7,44 @@ from google.genai.types import (
 )
 
 class BananaArtist(Artist):
-	def __init__(self, config: dict[str, str | int]) -> None:
-		super().__init__(config)
-		self.client = genai.Client()
+    def __init__(self, config: dict[str, Any]) -> None:
+        super().__init__(config)
+        self.client = genai.Client()
 
-	def paint(self, prompt: str, image_name: str) -> bool:
-		try:
-			# generate the image
-			image_response: GenerateContentResponse = self.client.models.generate_content(
-				model=self.config.get('model'),
-				contents=[prompt],
-				config=GenerateContentConfig(
-					image_config=ImageConfig(
-						aspect_ratio=self.config.get('aspect_ratio'),
-						#image_size='1K'
-					)
-				)
-			)
-		except Exception as e:
-			print(f'Banana Error: {e}')
-			return False
+    def paint(self, prompt: str, image_name: str, paint_cfg: dict[str, Any]) -> bool:
+        try:
+            # generate the image
+            model: str = self.config.get("model", "")
+            ar: str = paint_cfg.get("aspect_ratio", "1024x1024")
+            image_response: GenerateContentResponse = (
+                self.client.models.generate_content(
+                    model=model,
+                    contents=[prompt],
+                    config=GenerateContentConfig(
+                        image_config=ImageConfig(
+                            aspect_ratio=ar,
+                            # image_size='1K'
+                        )
+                    ),
+                )
+            )
+        except Exception as e:
+            print(f"Banana Error: {e}")
+            return False
 
-		if image_response.parts is None:
-			print('Banana Error: No proper response!')
-			return False
+        if image_response.parts is None:
+            print("Banana Error: No proper response!")
+            return False
 
-		for part in image_response.parts:
-			image = part.as_image()
-			if image is not None:
-				output_dir: Path = self.config.get('output_dir')
-				output_image_path: Path = output_dir / image_name
-				image.save(str(output_image_path.resolve()))
-				break
-		else:
-			print('Banana Error: No image was generated!')
-			return False
+        for part in image_response.parts:
+            image = part.as_image()
+            if image is not None:
+                output_dir: Path = self.config.get("output_dir")
+                output_image_path: Path = output_dir / image_name
+                image.save(str(output_image_path.resolve()))
+                break
+        else:
+            print("Banana Error: No image was generated!")
+            return False
 
-		return True
+        return True

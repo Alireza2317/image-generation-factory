547a3750b3750febd5af17d0e29be40abfbdb5cb
Alireza Alavi <alireza.alavi237@gmail.com>
2025-12-17 22:09:39 +0100

    improved base Artist paint method

--- artists/base_artist.py
+++ artists/base_artist.py
@@ -7,6 +7,11 @@ class Artist(ABC):
 		self.config = config
 
 	@abstractmethod
-	def paint(self, prompt: str, image_name: str, paint_cfg: dict[str, Any]) -> bool:
-		"""Returns True if image generation was successful, else False"""
+	def paint(
+		self, prompt: str, image_name_stem: str, paint_cfg: dict[str, Any]
+	) -> bool:
+		"""
+		image_name_stem: Desired filename WITHOUT extension.
+		paint_cfg: Per-image settings (aspect_ratio, style, etc.)
+		Returns True if image generation was successful, else False"""
 		pass

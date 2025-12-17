81eda7eea423787ee28addf50504a84567beb9a5
Alireza <alireza.alavi237@gmail.com>
2025-12-17 15:47:29 +0100

    update Brain to use ImageIdea as output

--- brains/base_brain.py
+++ brains/base_brain.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
-
+from typing import Any
+from core.models import ImageIdea
 
 class Brain(ABC):
 	@abstractmethod
-	def get_response(self, meta_prompt: str) -> dict[str, str] | None:
+	def get_response(self, meta_prompt: str) -> ImageIdea | None:
 		"""
 		Takes a meta prompt, which ideally has clear instructions for
-		the llm to output a json(dict) like this:
+		the llm to output a json like this:
 
 		{
 			"prompt": str,
@@ -14,12 +15,13 @@ class Brain(ABC):
 			"keywords": str,
 			"category": str
 		}
+		Then the class returns the ImageIdea object based on this json.
 
 		returns None in case of failiure
 		"""
 		pass
 
-	def validate_json(self, output: dict[str, str]) -> bool:
+	def validate_json(self, output: dict[str, Any]) -> bool:
 		"""
 		Checks if the output is really a valid json with desired keys.
 		"""

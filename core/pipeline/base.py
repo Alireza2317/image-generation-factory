7be9f5cdf50968e344a8a3530d5fff4062cb0c21
Alireza <alireza.alavi237@gmail.com>
2025-12-26 22:50:25 +0100

    added BasePipeline and Jobconfig bases

---
+++ core/pipeline/base.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from pydantic import BaseModel
+
+
+class JobConfig(BaseModel):
+	"""
+	Base model for job configurations.
+	Pipelines can subclass this to define their own configs.
+	"""
+
+	pass
+
+
+class BasePipeline(ABC):
+	@abstractmethod
+	def run_job(self, config: JobConfig) -> bool:
+		"""
+		Runs the full cycle for one single batch of images.
+		The implementation will be specific to each pipeline.
+		"""
+		pass

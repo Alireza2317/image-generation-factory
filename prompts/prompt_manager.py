f3a78bae5ae568b789e197c221589557226bbc12
Alireza <alireza.alavi237@gmail.com>
2025-12-27 22:58:48 +0100

    created  WildcardPromptManager to  yield prompts from files

--- prompts/prompt_manager.py
+++ prompts/prompt_manager.py
@@ -1,5 +1,4 @@
 from collections.abc import Generator
-import os
 from pathlib import Path
 
 
@@ -29,12 +28,14 @@ class MetaPromptManager:
 
 
 class WildcardPromptManager:
-	def __init__(self) -> None:
-		pass
+	def __init__(self, prompts_path: Path) -> None:
+		self.path = prompts_path
 
-	def prompts(self) -> Generator[str, None, None]:
-		yield "A futuristic __city__ at __time_of_day__ with flying __vehicle__s and __color__ lights."
-		yield "A serene __nature_location__ with a crystal-clear river and __forest_animal__s."
-		yield "A __sci_fi_job__ exploring an alien planet with __strange_adj__ rock formations."
-		yield "A majestic __castle__ on a hilltop overlooking a vast __fantasy_kingdom__."
-		yield "A bustling marketplace in a __fantasy_world__ with __colorful_adj__ stalls and __fantasy_character__s."
+	def prompts(self) -> Generator[tuple[str, str], None, None]:
+		for prompt_path in sorted(self.path.iterdir()):
+			with open(prompt_path, mode="r", encoding="utf-8") as file:
+				raw_prompt: str = file.read()
+
+			niche_name: str = prompt_path.stem
+
+			yield niche_name, raw_prompt
